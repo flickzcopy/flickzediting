ab011adbaa8d7188b69b991f303c1e98ca9ef076
pchwinner <pchwinnerr@gmail.com>
2025-12-15 10:54:21 +0100

    fix

--- server.js
+++ server.js
@@ -5331,7 +5331,7 @@ app.post('/api/users/login', async (req, res) => {
         res.cookie('userRefreshToken', refreshToken, {
             httpOnly: true, 
             secure: isProduction, 
-            sameSite: isProduction ? 'lax' : 'lax', 
+            sameSite: 'None', 
             maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
         });
         // --------------------------------------------------------
