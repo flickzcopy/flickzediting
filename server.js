3992730aebaaf36fe2a5f98d0bcbb1dd41bee8e4
pchwinner <pchwinnerr@gmail.com>
2025-12-31 09:30:16 +0100

    Fix

--- server.js
+++ server.js
@@ -1119,7 +1119,7 @@ const OrderItemSchema = new mongoose.Schema({
     variationIndex: { 
         type: Number, 
         required: [true, 'Variation index is required for inventory deduction.'],
-        min: 0 
+        min: 1 
     },    size: { type: String },
     color: { type: String },
     variation: { type: String } 
